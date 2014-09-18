179ba74235744029f87b52e5b6e294da4abe5468
Decesso <dawemolnar@gmail.com>
2014-09-18 20:03:18 +0200

    [m] added error throw if font is null

--- IState.cpp
+++ IState.cpp
@@ -1,6 +1,6 @@
 #include "IState.h"
 
-IState::IState(SDL_Window& window) : run_(true), win_(window) 
+IState::IState(SDL_Window& window) : font_(nullptr), run_(true), win_(window) 
 {
 	ren_ = SDL_CreateRenderer(&win_, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	if (ren_ == 0) {
@@ -16,6 +16,9 @@ IState::IState(SDL_Window& window) : run_(true), win_(window)
 void
 IState::renderText(const SDL_Color& color,SDL_Rect& destination, const std::string& text)
 {
+	if(font_ == nullptr) {
+		throw std::runtime_error("No font declared");
+	}
 	SDL_Surface *surf = TTF_RenderText_Blended(font_, text.c_str(), color);
 	SDL_Texture* score = SDL_CreateTextureFromSurface(ren_, surf);
 	SDL_FreeSurface(surf);
